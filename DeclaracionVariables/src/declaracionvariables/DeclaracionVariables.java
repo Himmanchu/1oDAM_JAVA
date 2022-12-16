0b50da23ce882e4b0583dddf07755326745e8bcc
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-12-16 11:58:19 +0100

    Commit

---
+++ DeclaracionVariables/src/declaracionvariables/DeclaracionVariables.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package declaracionvariables;
+
+/**
+ *
+ * @author javierjimenezsanz
+ */
+public class DeclaracionVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        boolean bool1;
+        bool1=true;
+        
+        boolean bool2=false;
+        System.out.println(bool1);
+        
+        System.out.println(bool2);
+        
+        int decimal1=100, decimal2= 200, decimal3 = 300;
+        
+        System.out.println(decimal1);
+        System.out.println(decimal2);
+        System.out.println(decimal3);
+        
+        float float1=15;
+        float float2=2.3f;
+        
+        System.out.println(float1);
+        System.out.println(float2);
+        
+        double double1 = 11.0;
+        
+        System.out.println(double1);
+        
+        byte byte1=10;
+        byte byte2=0xa;
+        
+        System.out.println(byte1);
+        System.out.println(byte2);
+    }
+    
+}
