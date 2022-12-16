736f0d1ee91530dcc38706570747cc84cd4d16d4
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-12-16 12:14:28 +0100

    Commit

---
+++ EjemploString/src/ejemplostring/EjemploString.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplostring;
+
+/**
+ *
+ * @author javierjimenezsanz
+ */
+public class EjemploString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String name= "Javi";
+        String example = "Tengo un texto 6789654";
+        
+        System.out.println("Nueva linea \n" + name);
+        System.out.println("Tabulador \t" + name);
+        System.out.println("Retroceso \b" +name);
+        System.out.println("Retorno de carro \r" +name);
+        System.out.println("Nueva linea \'" + name +"\'");
+        System.out.println(example);
+    }
+    
+}
