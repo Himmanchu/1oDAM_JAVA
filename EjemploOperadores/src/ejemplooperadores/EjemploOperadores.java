b10c8bd1a407a128b5668f9ab3b1d3f2d024a8a2
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-12-19 11:29:13 +0100

    Commit

---
+++ EjemploOperadores/src/ejemplooperadores/EjemploOperadores.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplooperadores;
+
+/**
+ *
+ * @author javierjimenezsanz
+ */
+public class EjemploOperadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a=10;
+        int b=15;
+        int c=20;
+        int d=25;
+        
+        System.out.println("a + b= "+ (a+b));
+        System.out.println("a - b = "+(a-b));
+        System.out.println("a * b = "+(a*b));
+        System.out.println("a / b = "+(a/b));
+        System.out.println("a % b = "+(a%b));
+        
+        System.out.println("a + 1 = "+(a+1));
+        System.out.println("d++ = "+(d++));
+        System.out.println("++d = "+(++d));
+    }
+    
+}
