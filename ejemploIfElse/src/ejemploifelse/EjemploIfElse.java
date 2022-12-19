7b5830e8570c09f6037fe690a052c18ca3bcf4bd
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-12-19 13:29:13 +0100

    Commit

---
+++ ejemploIfElse/src/ejemploifelse/EjemploIfElse.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemploifelse;
+
+/**
+ *
+ * @author javierjimenezsanz
+ */
+public class EjemploIfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Ejemplo If/Else
+        
+        int x=13;
+        
+        if(x==10){
+            System.out.println("El valor de x es 10");
+        }
+        else if(x==11){
+            System.out.println("El valor de x es 11");
+        }
+        else if(x==12){
+            System.out.println("El valor de x es 12");
+        }
+        else{
+            System.out.println("X es distinto a 10, a 11 y a 12. ES OTRO VALOR");
+        }
+    }
+    
+}
