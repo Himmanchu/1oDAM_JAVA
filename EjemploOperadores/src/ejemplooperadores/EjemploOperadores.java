43bd7c125afe4035ca64a561285606839de2a666
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-12-19 13:07:38 +0100

    Commit

--- EjemploOperadores/src/ejemplooperadores/EjemploOperadores.java
+++ EjemploOperadores/src/ejemplooperadores/EjemploOperadores.java
@@ -16,10 +16,11 @@ public class EjemploOperadores {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int a=10;
+        /*int a=10;
         int b=15;
         int c=20;
         int d=25;
+        */
         
        /* System.out.println("a + b= "+ (a+b));
         System.out.println("a - b = "+(a-b));
@@ -31,7 +32,7 @@ public class EjemploOperadores {
         System.out.println("d++ = "+(d++));
         System.out.println("++d = "+(++d));*/
        
-       System.out.println("a == b: " + (a==b));
+       /*System.out.println("a == b: " + (a==b));
        System.out.println("a != b: " + (a!=b));
        System.out.println("a > b: " + (a>b));
        System.out.println("!a > b: " + !(a>b));
@@ -39,7 +40,68 @@ public class EjemploOperadores {
        System.out.println("a >= b: " + (a>=b));
        System.out.println("a <= b: " + (a<=b));
        //b=10;
-       System.out.println("a >= b: " + (a>=b));
+       System.out.println("a >= b: " + (a>=b));*/
+       
+      /* boolean x =true;
+       boolean y=true;
+       boolean z= false;
+       boolean r=false;
+       
+       System.out.println(" x && z : " + (x&&z));
+       System.out.println(" x && y : " + (x&&y));
+       
+       System.out.println(" x || z : " + (x||z));
+       System.out.println(" r || z : " + (r||z));
+       
+       System.out.println(" !z : " + (!z));
+       System.out.println(" (x && y) : " + !(x&&y));
+       
+       System.out.println(" (x && y) || (z && y) : " + ((x && y) || (z && y)));*/
+      
+      /*int a=10;
+      int b=10;
+      int c=0;
+      
+      c=a+b;
+      System.out.println("c = a + b :: "+ c);
+      
+      c+=a;
+      System.out.println("c +=a :: " +c);
+      
+      c-=a;
+      System.out.println("c -=a :: " +c); 
+     
+      c*=a;
+      System.out.println("c *=a :: " +c);
+      
+      c/=a;
+      System.out.println("c /=a :: " +c);*/
+      
+      /*int x,z;
+      x=10;
+      
+      z=(x==1) ? 20:30;
+      System.out.println("Valor de z es :" +z);
+      
+      z=(x==10) ? 40:60;
+      System.out.println("Valor de z es: " +z);
+      */
+      
+      int x=5;
+      
+      System.out.println("Operador con x :" + ++x);
+      System.out.println("x : " + x);
+      System.out.println("Operador con x :" + x++);
+      System.out.println("x : " + x);
+      
+      System.out.println(4+5*6+7);
+      System.out.println((4+5)*(6+7));
+      
+      System.out.println( "Hola " +(5+6));
+      
+      
+      
+      
     }
     
 }
---
+++ EntradaSalida/src/entradasalida/EntradaSalida.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entradasalida;
+import java.util.Scanner;
+/**
+ *
+ * @author javierjimenezsanz
+ */
+public class EntradaSalida {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner entrada= new Scanner (System.in);
+        System.out.print("Introduce un número: ");
+        int numero = entrada.nextInt();
+        
+        System.out.println("El número que has puesto es: " +numero);
+        
+        /*System.out.print("Dime tu nombre: ");
+        String nombre = entrada.next();
+        System.out.println("Te llamas " + nombre);*/
+        
+        System.out.print("Dime tu altura: ");
+        float altura = entrada.nextFloat();
+        System.out.println("Tu altura por dos: " + 2* altura);
+        
+        System.out.print("Dime tu nombre completo: ");
+        entrada.nextLine();
+        String nombreCompleto = entrada.nextLine();
+        System.out.println("Tu nombre completo es: " +nombreCompleto);
+    }
+    
+}
