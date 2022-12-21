bb0029614f47b65db75cf7fac4625ab23ac34f9c
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-12-21 12:32:29 +0100

    Commit

--- Practica_Bucles/src/practica_bucles/Practica_Bucles.java
+++ Practica_Bucles/src/practica_bucles/Practica_Bucles.java
@@ -20,28 +20,47 @@ public class Practica_Bucles {
         int dia;
         int mes;
         String mesTexto="";
-        int año;
+        int ano;
         
         Scanner entradaDia = new Scanner (System.in);
-        System.out.print("Introduce un día del mes: ");
+        System.out.print("Introduce un día: ");
         dia = entradaDia.nextInt();
-        
-        
-        
+        while(dia<1||dia>31){
+            System.out.print("Este día no es válido. Introduce uno nuevo: ");
+            dia = entradaDia.nextInt();
+        }
+   
         Scanner entradaMes = new Scanner (System.in);
         System.out.print("Introduce mes: ");
         mes = entradaMes.nextInt();
         
-         while(mes>12 || mes<1){
-                      System.out.print("NO EXISTE ESTE MES. Introdúcelo otra vez.");
-        mes = entradaMes.nextInt();
+            while(((dia==31) && (mes==4 || mes==6 || mes==9 || mes==11)) || ((dia>28) && (mes==2))){
+                switch(mes){
+                    case 4, 6, 9, 11:
+                        System.out.print("Este mes solo tiene 30 días. Introduce otro mes: ");
+                        mes = entradaMes.nextInt();
+                        break;
+                    case 2:
+                        System.out.print("Febrero tiene 28 días. Introduce otro mes: ");
+                        mes = entradaMes.nextInt();
+                        break;
+                    default:
+                        mes = entradaMes.nextInt();
+                }
+                
+                   while(mes>12 || mes<1){
+                      System.out.print("NO EXISTE ESTE MES. Introdúcelo otra vez: ");
+                        mes = entradaMes.nextInt();
             }
+                    }
+              //AL PONER EL WHILE ANTES DEL SWITCH, EL DEFAULT NO ME HARÍA FALTA PORQUE YA COMPLETA LAS OPCIONES DE MESES      
+            
             switch(mes){
                 case 1 : 
                     mesTexto= "Enero";
                     break;
                 case 2 : 
-                    mesTexto= "Febrero";
+                    mesTexto= "Febrero"; 
                     break;    
                 case 3 : 
                     mesTexto= "Marzo";
@@ -73,15 +92,20 @@ public class Practica_Bucles {
                 case 12 : 
                     mesTexto= "Diciembre";
                     break;
-                    
-          //AL PONER EL WHILE ANTES DEL SWITCH, EL DEFAULT NO ME HARÍA FALTA PORQUE YA COMPLETA LAS OPCIONES DE MESES      
-               // default : 
-                 //   mesTexto= "NO EXISTE ESTE MES";
+                   
+                    }
+                      
+            
+            Scanner entradaAno = new Scanner (System.in);
+            System.out.print("Introduce año: ");
+            ano = entradaAno.nextInt();
+                while(ano<0){
+                    System.out.print("El año no puede ser inferior a 0. Introduce otro año: ");
+                    ano = entradaAno.nextInt();
+                }
+                System.out.println("La fecha introducida es: "+ dia + " de " + mesTexto + " de " + ano +".");
+         }
 
     }
-           System.out.print(mesTexto);
-        
-    
-    }
-}
+
 
